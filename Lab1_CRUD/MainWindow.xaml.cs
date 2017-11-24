2126f54acba0e55f97b8aa2c17a798b6c50eb851
Simone1989 <31948425+Simone1989@users.noreply.github.com>
2017-11-24 10:29:36 +0100

    Added a lot of stuff

--- Lab1_CRUD/MainWindow.xaml.cs
+++ Lab1_CRUD/MainWindow.xaml.cs
@@ -22,11 +22,13 @@ namespace Lab1_CRUD
     /// </summary>
     public partial class MainWindow : Window
     {
-        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Cinema;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=D:\SKRIVBORD\CINEMA.MDF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         public MainWindow()
         {
             InitializeComponent();
             PopulateDirectorListBox();
+            // MÅSTE KOLLA UPP
+            //btnInsertDirector.IsEnabled = !string.IsNullOrWhiteSpace(txtDirectorId.Text);
         }
 
         // Metod to populate the director listbox
@@ -36,8 +38,9 @@ namespace Lab1_CRUD
             {
                 try
                 {
+                    // När man använder whilen och reader.read ska man ladda in alla värden, som sedan ploppas in i objekt. 
                     connection.Open();
-                    string query = "SELECT Fullname FROM Directors";
+                    string query = "SELECT Id, Fullname FROM Directors";
                     SqlCommand command = new SqlCommand(query, connection);
                     SqlDataReader reader = command.ExecuteReader();
                     while (reader.Read())
@@ -56,6 +59,7 @@ namespace Lab1_CRUD
             }
         }
 
+
         private void PopulateMovieListBox()
         {
             listMovies.Items.Clear();
@@ -64,9 +68,9 @@ namespace Lab1_CRUD
                 try
                 {
                     connection.Open();
-                    // FUNGERAR INTE SOM DET SKA ÄN, LÄSER IN EN FILMTITEL I TAGET, GÅR IGENOM LISTAN MED WHILEN
-                    string query = "SELECT * FROM Movies INNER JOIN Directors ON Directors.Id = Movies.DirectorId WHERE Movies.Id = '" + txtDirectorId.Text + "' ";
+                    string query = "SELECT * FROM Movies INNER JOIN Directors ON Directors.Id = Movies.DirectorId WHERE DirectorId = @directorId";
                     SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@directorId", this.txtDirectorId.Text);
                     SqlDataReader reader = command.ExecuteReader();
                     while (reader.Read())
                     {
@@ -89,8 +93,9 @@ namespace Lab1_CRUD
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                string query = "SELECT * FROM Directors WHERE Fullname = '" + listDirector.SelectedItem + "' ";
+                string query = "SELECT * FROM Directors WHERE Fullname = @fullname";
                 SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@fullname", listDirector.SelectedItem ?? (object)DBNull.Value);
                 SqlDataReader reader;
                 try
                 {
@@ -99,14 +104,13 @@ namespace Lab1_CRUD
 
                     while (reader.Read())
                     {
-                        // FUNGERAR INTE SOM DET SKA ÄN MED BIRTHDAY
                         string id = reader.GetInt32(0).ToString();
                         string fullanme = reader.GetString(1);
-                        //string birthday = reader.GetInt32(2).ToString();
+                        string birthday = reader.GetDateTime(2).Date.ToShortDateString();
 
                         txtDirectorId.Text = id;
                         txtDirectorName.Text = fullanme;
-                        //txtDirectorName.Text = birthday;
+                        txtDirectorBirthday.Text = birthday;
                     }
                 }
                 catch (SqlException ex)
@@ -124,8 +128,9 @@ namespace Lab1_CRUD
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                string query = "SELECT * FROM Movies WHERE Title = '" + listMovies.SelectedItem + "' ";
+                string query = "SELECT * FROM Movies WHERE Title = @title";
                 SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue(@"title", listMovies.SelectedItem ?? (object)DBNull.Value);
                 SqlDataReader reader;
                 try
                 {
@@ -135,10 +140,12 @@ namespace Lab1_CRUD
                     while (reader.Read())
                     {
                         // FÖRSÖK JOINA TABELLERNA SÅ ATT DET STÅR ETT NAMN I DIRECTOR ISTÄLLET FÖR EN SIFFRA
+                        string id = reader.GetInt32(0).ToString();
                         string title = reader.GetString(1);
                         string releaseDate = reader.GetInt32(2).ToString();
                         string director = reader.GetInt32(3).ToString();
 
+                        txtMovieId.Text = id;
                         txtMovieTitle.Text = title;
                         txtMovieReleaseDate.Text = releaseDate;
                         txtMovieDirector.Text = director;
@@ -155,7 +162,7 @@ namespace Lab1_CRUD
             }
         }
 
-        // Button clicks
+        // Button clicks director
         private void btnInsertDirector_Click(object sender, RoutedEventArgs e)
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -163,11 +170,14 @@ namespace Lab1_CRUD
                 try
                 {
                     connection.Open();
-                    string query = "INSERT INTO Directors (Fullname, Birthday) VALUES ('" + this.txtDirectorName.Text + "' , '" + this.txtDirectorBirthday.Text + "')";
+                    string query = "INSERT INTO Directors (Fullname, Birthday) VALUES (@name, @birthday)";
                     SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@birthday", this.txtDirectorBirthday.Text);
+                    command.Parameters.AddWithValue("@name", this.txtDirectorName.Text);
                     command.ExecuteNonQuery();
+
                     listDirector.Items.Add(this.txtDirectorName.Text);
-                    MessageBox.Show("Saved");
+                    MessageBox.Show("Saved.");
 
                 }
                 catch (Exception ex)
@@ -188,10 +198,11 @@ namespace Lab1_CRUD
                 try
                 {
                     connection.Open();
-                    string query = "UPDATE Directors SET Id = '" + this.txtDirectorId.Text + "' ," +
-                        " Fullname = '" + this.txtDirectorName.Text + "', Birthday = '" + this.txtDirectorBirthday.Text + 
-                        "' WHERE Id = '" + this.txtDirectorId.Text + "' ";
+                    string query = "UPDATE Directors SET Fullname = @name, Birthday = @birthday WHERE Id = @id";
                     SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@birthday", this.txtDirectorBirthday.Text);
+                    command.Parameters.AddWithValue("@name", this.txtDirectorName.Text);
+                    command.Parameters.AddWithValue("@id", this.txtDirectorId.Text);
                     command.ExecuteNonQuery();
                     MessageBox.Show("Updated");
 
@@ -205,32 +216,195 @@ namespace Lab1_CRUD
                     connection.Close();
                 }
             }
+            listDirector.Items.Clear();
+            PopulateDirectorListBox();
         }
 
-        // What happens when something is written in the director id textbox
+        private void btnDeleteDirector_Click(object sender, RoutedEventArgs e)
+        {
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "DELETE FROM Directors WHERE Id = @id";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue(@"id", txtDirectorId.Text);
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Successfully deleted.");
+                    txtDirectorId.Text = "";
+                    txtDirectorName.Text = "";
+                    txtDirectorBirthday.Text = "";
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+            listDirector.Items.Clear();
+            PopulateDirectorListBox();
+        }
+
+        // Button clicks movies
+        private void btnInsertMovie_Click(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    // HÄR KOMMER ETT FEL NÄR MAN INSERTAR FILMER UTAN ATT MAN HAR KLICKAT PÅ NÅTT ANNAT FÖRST
+                    connection.Open();
+                    string query = "INSERT INTO Movies (Title, ReleaseYear, DirectorId) VALUES (@title, @year, @director)";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@title", this.txtMovieTitle.Text);
+                    command.Parameters.AddWithValue("@year", this.txtMovieReleaseDate.Text);
+                    command.Parameters.AddWithValue("@director", this.txtMovieDirector.Text);
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Saved.");
+                    listMovies.Items.Clear();
+                    PopulateMovieListBox();
+                    ClearTextboxes();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        private void btnUpdateMovie_Click(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "UPDATE Movies SET Title = @title, ReleaseYear = @year, DirectorId = @directorId WHERE Id = @movieId";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@title", this.txtMovieTitle.Text);
+                    command.Parameters.AddWithValue("@year", this.txtMovieReleaseDate.Text);
+                    command.Parameters.AddWithValue("@directorId", this.txtMovieDirector.Text);
+                    command.Parameters.AddWithValue("@movieId", this.txtMovieId.Text);
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Updated.");
+                    listMovies.Items.Clear();
+                    PopulateMovieListBox();
+                    ClearTextboxes();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        private void btnDeleteMovie_Click(object sender, RoutedEventArgs e)
+        {
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "DELETE FROM Movies WHERE Id = @movieId";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@movieId", this.txtMovieId.Text);
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Successfully deleted.");
+                    listMovies.Items.Clear();
+                    PopulateMovieListBox();
+                    ClearTextboxes();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        private void btnClearTextboxes_Click(object sender, RoutedEventArgs e)
+        {
+            txtDirectorId.Text = "";
+            txtDirectorName.Text = "";
+            txtDirectorBirthday.Text = "";
+            txtMovieDirector.Text = "";
+            txtMovieReleaseDate.Text = "";
+            txtMovieTitle.Text = "";
+            txtMovieId.Text = "";
+        }
+
         private void txtDirectorId_TextChanged(object sender, TextChangedEventArgs e)
         {
             //btnInsertDirector.IsEnabled = !string.IsNullOrWhiteSpace(txtDirectorId.Text);
+            if(txtDirectorName.Text == "")
+            {
+                btnInsertDirector.IsEnabled = true;
+            }
         }
 
-        // What happens when a selection is changed in the director listbox
         private void listDirector_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            PopulateDirectorTextboxes();
+            PopulateMovieListBox();
+
+            // Buttons get enabled/disabled depending on selections in listboxes
             if (listDirector.SelectedIndex != -1)
             {
                 btnDeleteDirector.IsEnabled = true;
                 btnUpdateDirector.IsEnabled = true;
+                btnDeleteMovie.IsEnabled = false;
+                btnUpdateMovie.IsEnabled = false;
+                ClearTextboxes();
             }
-            PopulateDirectorTextboxes();
-            PopulateMovieListBox();
-            // Här ska director info dyka upp i director textboxarna
-            // Filmer som är kopplade till directorn ska även visas i Movie-listboxen.
-
         }
 
         private void listMovies_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             PopulateMovieTextboxes();
+
+            // Buttons get enabled/disabled depending on selections in listboxes
+            if (listMovies.SelectedIndex != -1)
+            {
+                btnDeleteMovie.IsEnabled = true;
+                btnUpdateMovie.IsEnabled = true;
+                btnDeleteDirector.IsEnabled = false;
+                btnUpdateDirector.IsEnabled = false;
+                ClearTextboxes();
+            }
         }
+
+        private void ClearTextboxes()
+        {
+            if(listMovies.SelectedIndex != -1)
+            {
+                //txtDirectorId.Text = "";
+                //txtDirectorName.Text = "";
+                //txtDirectorBirthday.Text = "";
+            }
+            else if(listDirector.SelectedIndex != -1)
+            {
+                txtMovieDirector.Text = "";
+                txtMovieReleaseDate.Text = "";
+                txtMovieTitle.Text = "";
+                txtMovieId.Text = "";
+            }
+        }
+
     }
 }
---
+++ Lab1_CRUD/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_CRUD
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+        public int DirectorId { get; set; }
+    }
+}
